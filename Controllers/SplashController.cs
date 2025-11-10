402ac10f63b732672694255df0dd3beaf06c452e
Zulfiqor Rustamov <zulfiqorrustamov81@gmail.com>
2025-11-11 00:23:55 +0500

    Add Splash view

---
+++ Controllers/SplashController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MiniOnlineStore.Controllers;
+
+public class SplashController(
+    ILogger<SplashController> logger) : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
